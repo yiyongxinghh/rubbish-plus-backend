50024c1bc8379c12562a0f641542d9b2d058eb9d
zxw <zhangxuanweihh@outlook.com>
2024-03-13 16:23:58 +0800

    评论功能补全

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -5,37 +5,97 @@ import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Controller('comment')
 export class CommentController {
-  constructor(private readonly commentService: CommentService) {}
+  constructor(private readonly commentService: CommentService) { }
 
+  /**
+   * POST 创建评论
+   * @param createCommentDto 
+   * @returns 
+   */
   @Post()
   create(@Body() createCommentDto: CreateCommentDto) {
     return this.commentService.create(createCommentDto);
   }
 
+  /**
+   * GET 获取所有评论，自带分页
+   * @param query 
+   * @returns 
+   */
   @Get()
-  async findAll(@Query() query: { page: number,pageSize: number }) {
+  async findAll(@Query() query: { page: number, pageSize: number }) {
     const { page, pageSize } = query
     const comments = await this.commentService.findAll(page, pageSize);
     const total = await this.commentService.getTotal()
-    return {comments,total}
+    return { comments, total }
   }
 
+  /**
+   * 
+   * @param query 
+   * @returns 
+   */
+  @Get('/hot')
+  async findHotAll(@Query() query: { page: number, pageSize: number, id: number }) {
+    const { page, pageSize, id } = query
+    const comments = await this.commentService.findHotAll(page, pageSize, id);
+    const total = await this.commentService.getTotal(id)
+    return { comments, total }
+  }
+
+  /**
+   * 
+   * @param query 
+   * @returns 
+   */
+  @Get('/new')
+  async findNewAll(@Query() query: { page: number, pageSize: number, id: number }) {
+    const { page, pageSize, id } = query
+    const comments = await this.commentService.findNewAll(page, pageSize, id);
+    const total = await this.commentService.getTotal(id)
+    return { comments, total }
+  }
+
+
+
+  /**
+   * POST 根据起始时间范围，获取区间所有评论
+   * @param date 
+   * @returns 
+   */
   @Post('time')
   findTimed(@Body() date: { start: Date, end: Date }) {
     const { start, end } = date
     return this.commentService.findTimed(start, end);
   }
 
+  /**
+   * GET 根据指定id获取指定评论
+   * @param id 
+   * @returns 
+   */
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.commentService.findOne(+id);
   }
 
+  /**
+   * PATCH 根据指定id更新指定评论
+   * @param id 
+   * @param updateCommentDto 
+   * @returns 
+   */
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
     return this.commentService.update(+id, updateCommentDto);
   }
 
+  /**
+   * DELETE 根据指定id移除指定评论
+   * @param id 
+   * @param updateCommentDto 
+   * @returns 
+   */
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.commentService.remove(+id);
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -9,32 +9,97 @@ import { Repository } from 'typeorm';
 export class CommentService {
   constructor(@InjectRepository(Comment) private readonly comment: Repository<Comment>) { }
 
+  /**
+   * 创建评论
+   * @param createCommentDto 
+   * @returns 
+   */
   create(createCommentDto: CreateCommentDto) {
     return this.comment.createQueryBuilder().insert().into(Comment).values(createCommentDto).execute();
   }
 
+  /**
+   * 获取所有评论，自带分页
+   * @param page 
+   * @param pageSize 
+   * @returns 
+   */
   findAll(page: number, pageSize: number) {
     if (page && pageSize) { return this.comment.createQueryBuilder().skip(pageSize * (page - 1)).take(pageSize).getMany(); }
     else { return this.comment.createQueryBuilder().getMany(); }
   }
 
+  /**
+   * 获取最热评论，自带分页
+   * @param page 
+   * @param pageSize 
+   * @returns 
+   */
+  findHotAll(page: number, pageSize: number, id: number) {
+    if (page && pageSize) { return this.comment.createQueryBuilder().skip(pageSize * (page - 1)).take(pageSize).where('garbage_id = :id', { id }).orderBy('comment_score', 'DESC').getMany(); }
+    else { return this.comment.createQueryBuilder().where('garbage_id = :id', { id }).orderBy('comment_score', 'DESC').getMany(); }
+  }
+
+  /**
+   * 获取最新评论，自带分页
+   * @param page 
+   * @param pageSize 
+   * @returns 
+   */
+  findNewAll(page: number, pageSize: number, id: number) {
+    if (page && pageSize) { return this.comment.createQueryBuilder().skip(pageSize * (page - 1)).take(pageSize).where('garbage_id = :id', { id }).orderBy('comment_time', 'DESC').getMany(); }
+    else { return this.comment.createQueryBuilder().where('garbage_id = :id', { id }).orderBy('comment_time', 'DESC').getMany(); }
+  }
+
+  /**
+   * 根据起始时间范围，获取区间所有评论
+   * @param start 
+   * @param end 
+   * @returns 
+   */
   findTimed(start: Date, end: Date) {
     return this.comment.createQueryBuilder().where("comment_time >= :start AND comment_time <= :end", { start, end }).getMany();
   }
 
+  /**
+   * 根据指定id获取指定评论
+   * @param id 
+   * @returns 
+   */
   findOne(id: number) {
     return this.comment.createQueryBuilder().where("comment.id = :id", { id }).getOne();
   }
 
+  /**
+   * 根据指定id更新指定评论
+   * @param id 
+   * @param updateCommentDto 
+   * @returns 
+   */
   update(id: number, updateCommentDto: UpdateCommentDto) {
     return this.comment.createQueryBuilder().update(Comment).set(updateCommentDto).where("comment.id = :id", { id }).execute();
   }
 
+  /**
+   * 根据指定id移除指定评论
+   * @param id 
+   * @returns 
+   */
   remove(id: number) {
     return this.comment.createQueryBuilder().delete().from(Comment).where("comment.id = :id", { id }).execute();
   }
 
-  getTotal() {
-    return this.comment.createQueryBuilder().getCount() // 获取总数量
+  /**
+   * 获取评论表总数，可携带id
+   * @param id 
+   * @returns 
+   */
+  getTotal(id?: number) {
+    if (id) { 
+      return this.comment.createQueryBuilder().where('garbage_id = :id', { id }).getCount()
+    }
+    else {
+      return this.comment.createQueryBuilder().getCount() // 获取总数量
+    }
   }
 }
